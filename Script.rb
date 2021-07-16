9b6af06ee80d8ada9cddade218d3d1f888a113cf
ctrlVee <me123opportunity@gmail.com>
2021-07-16 14:45:40 -0400

    Adds key and returns as new strng

--- Script.rb
+++ Script.rb
@@ -4,6 +4,13 @@ def cipher(txt, key)
  
   broken = txt.bytes #returns ascii code broken in to array
   
+  addKey = broken.map do |ascii|
+    new_letter = (ascii + key).chr
+  end
+
+  combine_key = addKey.join
+  
+
   binding.pry
 end
 
