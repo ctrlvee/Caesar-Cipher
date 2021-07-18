027b22cee69f6efae7ebee8fcc2ee9e4899cc95e
ctrlVee <me123opportunity@gmail.com>
2021-07-18 19:09:54 -0400

    Succesfully adds letters in a-z & A-Z

--- Script.rb
+++ Script.rb
@@ -7,13 +7,38 @@ def cipher(txt, key)
   returnWithkey()
 end
 
+def addToUpper(og_ascii)
+  new_ascii = og_ascii
+  @key.times do
+    new_ascii += 1
+    if new_ascii > 90
+      new_ascii = 65
+    end
+  end
+  new_ascii.chr
+end
+
+def addToLower(og_ascii)
+ # binding.pry
+  new_ascii = og_ascii
+  @key.times do
+    new_ascii += 1
+    if new_ascii > 122
+      new_ascii = 97
+    end
+  end
+  new_ascii.chr
+end
+
 #Update with correct conditional to identify if "og_ascii" exists between the values
 def checkPosition (og_ascii)
   if og_ascii.between?(65,90)
-    puts 'upper'
+    addToUpper(og_ascii)
   elsif og_ascii.between?(97,122)
-    puts 'lower'
-
+    addToLower(og_ascii)
+  else #returns whitespac/punctuation as the same
+    og_ascii.chr
+    binding.pry
   end
 end
 
@@ -21,8 +46,9 @@ def returnWithkey
   add_key = @ascii_txt.map do |og_ascii|
     checkPosition(og_ascii)
   end
-
+  puts add_key.join()
 end
 
-binding.pry
-cipher('ZZZest',4)
+
+
+cipher('Wow! Don/t',4)
